9eee16922532389959268729b008d7c6d6dee704
ocaiorod <crodriguesdesign@outlook.com>
2026-02-26 13:47:45 -0300

    feat: creating page structure and modeling footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button"
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
  
 const Page = () => {
   return (
-    <div>
-      <Button>Click aqui</Button>
-
-      <ThemeToggle />
+    <div className="w-full max-w-4xl mx-auto">
+      <Header />
+      <div className="mx-3">
+        ...
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import { Separator } from "./ui/separator";
+
+export const Footer = () => {
+    return (
+        <footer className="mt-5">
+            <Separator />
+            <div className="my-5 text-center text-sm opacity-50">
+                Criado por Caio Rodrigues
+            </div>
+        </footer>
+    );
+}
